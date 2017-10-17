4349ccb361b2ad46ceba92de69ee349a97de6927
stefan0722 <stefan.henneberger@salzburgresearch.at>
2017-10-17 13:14:36 +0200

    update scripts and increase version

--- script/create-feature-branch.bat
+++ script/create-feature-branch.bat
@@ -7,20 +7,27 @@ echo Switch local repository to develop branch
 git checkout develop
 echo ... done
 
+echo Retrieve all changes from GitHub
+git pull origin develop
+
+SET /P handwork="Do you need to do some manual merges [y/n]?"
+IF %handwork% == "y" GOTO :exit
+
 echo Files that should be commited before resuming:
 git status -s
 
 :: commit already not commited files to develop branch (OPTIONAL). If not done, the not commited Files
 :: will move the feature branch as not commited files
 echo Commit all local changes
-SET /P confirm="Should the changes be commited [y/n]?"
-IF %confirm% == "y" git commit -a
+SET /P confirm_1="Should the changes be commited [y/n]?"
+IF %confirm_1% == "n" GOTO :exit
+git commit -a
 
 :: push all commited Files to the develop branch
 :: make sure that all your changes are on the dev branch
 echo Pushing changes to develop branch
-SET /P confirm="Should the commited changes be pushed to github [y/n]?"
-IF %confirm% == "y" git push origin develop
+SET /P confirm_2="Should the commited changes be pushed to github [y/n]?"
+IF %confirm_2% == "y" git push origin develop
 
 :: crate new feature branch locally
 echo Create new Feature branch locally
@@ -41,3 +48,6 @@ echo Pushing Feature branch to Github
 SET /P confirm="Should the feature branch be pushed to gitgub [y/n]"
 IF %confirm% == "y" git push origin feature-%feature_branch_name%
 endlocal
+
+echo Feature Branch batch job ended
+:exit
--- script/finish-feature-branch.bat
+++ script/finish-feature-branch.bat
@@ -1,14 +1,43 @@
 @echo off
 setlocal
 
-::this script merges the feature branch back to develop
-git checkout feature-??
+git show-branch --list feature-*
+echo .
+SET /P feature_branch="Please enter name of feature branch [feature-]"
 
+echo Change to feature branch
+git checkout feature-%feature_branch%
+
+echo Pull current version from GitHub
+git pull origin feature-%feature_branch%
+
+echo Status of branch:
 git status -s
 
+SET /P handwork="Do you need to do some manual merges [y/n]?"
+IF %handwork% == "y" GOTO :exit
+
+echo Commiting Changes to local feature branch
 git commit -a
 
-git push origin feature-??
+echo Pushing changes to GitHub
+git push origin feature-%feature_branch%
 
+echo Change to develop branch
+git checkout develop
 
+echo Get all changes from Github
+git pull origin develop
 
+echo Increasing version of develop branch
+"%M2_HOME%/bin/mvn" release:update-versions
+
+echo Commiting update POM Files
+:: commit the pom file with the updated version
+git commit -m "changing develop version to new version" *pom.xml
+echo ... done
+
+endlocal
+
+echo Feature Branch batch job ended
+:exit
