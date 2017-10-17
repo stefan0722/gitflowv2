28279a427ff8285e73864dcab89c3d2d66f6f50b
stefan0722 <stefan.henneberger@salzburgresearch.at>
2017-10-17 14:02:35 +0200

    Scripts for GitFlow Updatec

--- script/create-feature-branch.bat
+++ script/create-feature-branch.bat
@@ -1,41 +1,7 @@
 @echo off
 setlocal
-cls
-:: This is a script for the GIT Flow process to create a new feature branch
 
-echo Retrieve all changes from GitHub
-git pull origin develop
-echo.
-echo.
-:: first change to your develop branch
-echo.
-echo.
-echo Switch local repository to develop branch
-git checkout develop
-echo.
-echo.
-SET /P handwork="Do you need to do some manual merges [y/n]?"
-IF /I "%handwork%" == "y" goto exit
-echo.
-echo.
-echo Files that should be commited before resuming:
-git status -s
-echo.
-echo.
-:: commit already not commited files to develop branch (OPTIONAL). If not done, the not commited Files
-:: will move the feature branch as not commited files
-echo Commit all local changes
-SET /P confirm_1="Should the changes be commited [y/n]?"
-IF /I "%confirm_1%" == "n" goto exit
-git commit -a
-
-echo.
-echo.
-:: push all commited Files to the develop branch
-:: make sure that all your changes are on the dev branch
-echo Pushing changes to develop branch
-SET /P confirm_2="Should the commited changes be pushed to github [y/n]?"
-IF "%confirm_2%" == "y" git push origin develop
+call before-branch.bat
 
 echo.
 echo.
