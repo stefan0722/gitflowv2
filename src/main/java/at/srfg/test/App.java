9c5e3af56c323820e9b35f6c3c09668604c6a32c
stefan0722 <stefan.henneberger@salzburgresearch.at>
2017-10-17 09:50:54 +0200

    first initial commit

---
+++ src/main/java/at/srfg/test/App.java
@@ -0,0 +1,13 @@
+package at.srfg.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
