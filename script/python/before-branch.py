3a3daef540cdc0a76ab4a666850333e5847a5d02
stefan0722 <stefan.henneberger@salzburgresearch.at>
2017-10-19 14:40:08 +0200

    further commit

--- script/python/before-branch.py
+++ script/python/before-branch.py
@@ -1,36 +1,27 @@
 import subprocess
 
-import os
-
-# exec(open("create-feature-branch.py").read())
-# print(os.environ.get('M2_HOME'))
 print("-- Step 1:     Change local repository to develop branch --")
 ahead = subprocess.check_output(["git","checkout","develop"]).decode("utf-8")
-print(ahead)
-print()
-print()
+print(ahead + "\n\n")
+
 print("-- Step 2:     Show Status of branch --")
 notCommited = subprocess.check_output(["git","status","-s"]).decode("utf-8")
-print(notCommited)
-print()
-print()
+print(notCommited + "\n")
 if notCommited is not '' :
-    eingabe = input("Sollen die Änderungen local commited werden? [Y/N]: ")
+    eingabe = input("Should the changes be commited to local repository? [Y/N]: ")
     if eingabe is "Y" :
-        message = input("Bitte Commit message eingeben: ")
+        message = input("Please enter commit message: ")
         subprocess.call(["git","commit","-a","-m",message])
-print()
-print()
+print("\n\n")
+
 print("-- Step 3:     Pull current version of develop from GitHub --")
 upToDate = subprocess.check_output(["git","pull","origin","develop"]).decode("utf-8")
 print(upToDate)
-if "Already up-to-date" not in str(upToDate) :
-    exit("Please control and merge develop branch")
 
-if "git push" in ahead or notCommited.splitlines().__sizeof__() > 0 :
-    eingabe1 = input("Sollen die Änderungen (Commits) nach develop gepusht werden? [Y/N]")
+if "Already up-to-date" not in str(upToDate) :
+    exit("Please check pulled changes and reexecute this script again")
+
+if "git push" in ahead or notCommited is not '' > 0 :
+    eingabe1 = input("Should all commits be pushed to GitHub? [Y/N]")
     if eingabe1 is "Y" :
         subprocess.call(["git","push","origin","develop"])
-print()
-print()
-
--- script/python/create-feature-branch.py
+++ script/python/create-feature-branch.py
@@ -1,1 +1,6 @@
-print("Juhuuuu !")
+import os
+
+exec(open("before-branch.py").read())
+
+
+# print(os.environ.get('M2_HOME'))
