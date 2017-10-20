daa534d3515ad730447a1ab86ef8f77eaabe4a45
stefan0722 <stefan.henneberger@salzburgresearch.at>
2017-10-20 14:57:00 +0200

    push changes update

--- script/python/p01-standard-push.py
+++ script/python/p01-standard-push.py
@@ -24,7 +24,7 @@ errorCode = subprocess.call(["git","pull","origin",branchName])
 if errorCode is not 0 :
     exit("Please resolve CONFLICTS and try again")
 
-print("\n-- Step 3:       Increasing version of develop branch")
+print("\n-- Step 3:       Increasing version of " + branchName + " branch")
 projectDir = input("Please enter project directory: ")
 subprocess.call([os.environ.get('M2_HOME')+"/bin/mvn","versions:set",
                  "-f=" + projectDir,
@@ -32,10 +32,10 @@ subprocess.call([os.environ.get('M2_HOME')+"/bin/mvn","versions:set",
                  "-DprocessAllModules=true",
                  "-DgenerateBackupPoms=false"],shell=True)
 
-print("\n-- Step 4:       Commiting update POM Files to develop branch")
+print("\n-- Step 4:       Commiting update POM Files to " + branchName + " branch")
 subprocess.call(["git","commit","-m","Change develop branch version to next SNAPSHOT version",projectDir + "**pom.xml"])
 
-print("\n-- Step 5:       Push the changes with version change to GitHub")
+print("\n-- Step 5:       Push all changes to GitHub")
 eingabe1 = input("Should all commits be pushed to GitHub? [Y/N]: ")
 if eingabe1 is "Y" :
     remoteUrl = subprocess.check_output(["git","config","--get","remote.origin.url"]).decode("utf-8").replace("\n","")
