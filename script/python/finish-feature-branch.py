2dce19157b40491af3f943aa04f968b4af8eafae
stefan0722 <stefan.henneberger@salzburgresearch.at>
2017-10-20 13:27:04 +0200

    update script

--- script/python/finish-feature-branch.py
+++ script/python/finish-feature-branch.py
@@ -1,4 +1,4 @@
-import subprocess, getpass
+import subprocess, getpass, webbrowser
 
 print("\nAvailable local feature branches: \n")
 subprocess.call(["git","show-branch","--list","feature-*"])
@@ -19,6 +19,18 @@ upToDate = subprocess.check_output(["git","pull","origin","feature-"+ featureBra
 if "Already up-to-date" not in str(upToDate) :
     exit("Please check pulled changes and reexecute this script again")
 
+# Bring develop branch in sync
+exec(open("before-branch.py").read())
+
+print("\n-- Step 3:     Change local repository to feature-"+ featureBranchName +" branch --")
+subprocess.call(["git","checkout","feature-" + featureBranchName],shell=True)
+
+print("\n-- Step 4:     Change local repository to feature-"+ featureBranchName +" branch --")
+mergeResult = subprocess.check_output(["git","merge","develop"]).decode("utf-8")
+print(mergeResult+ "\n")
+if "CONFLICT" in mergeResult :
+    exit("Please resolve conflict before continue")
+
 print("-- Step 5:     Pushing changes of feature-" + featureBranchName + " to GitHub --")
 eingabe1 = input("Should all commits be pushed to GitHub? [Y/N]: ")
 if eingabe1 is "Y" :
@@ -27,3 +39,6 @@ if eingabe1 is "Y" :
     password = getpass.getpass("Please enter password for " + remoteUrl + ": ")
     remoteUrl = remoteUrl.replace("https://github.com/","https://" + username + ":" + password + "@github.com/")
     subprocess.call(["git","push",remoteUrl,"feature-" + featureBranchName])
+
+    print("Please Open Pull Request")
+    webbrowser.open_new(remoteUrl + "/compare/feature-" + featureBranchName+ "?expand=1")
