42faf0b97c2672b37b68b2134e72e63adb46ff6d
stefan0722 <stefan.henneberger@salzburgresearch.at>
2017-10-23 15:21:34 +0200

    add env variables

--- script/python/before-branch.py
+++ script/python/before-branch.py
@@ -1,19 +1,45 @@
 #!/usr/bin/python
 
-import subprocess, getpass
+import subprocess, getpass, os
+
+M2_HOME = os.environ.get("M2_HOME")
+PROJECT_HOME = ""
+
+
+def load_environment_var() :
+    with open("environment.txt","r") as f:
+        for line in f.read().splitlines() :
+            elements = line.split('=')
+            key = elements[0]
+            value = elements[1]
+            if key is "M2_HOME":
+                global M2_HOME
+                M2_HOME = value
+            if key is "PROJECT_HOME":
+                global PROJECT_HOME
+                PROJECT_HOME = value
+
+
+def checkout_branch(branch):
+    return subprocess.check_output(["git","checkout",branch]).decode("utf-8")
+
+
+def commit_changes():
+    not_commited = subprocess.check_output(["git","status","-s"]).decode("utf-8")
+    print(not_commited + "\n")
+    if not_commited is not '' :
+        eingabe = input("Should the changes be commited to local repository? [Y/N]: ")
+        if eingabe is "Y" :
+            message = input("Please enter commit message: ")
+            subprocess.call(["git","commit","-a","-m",message])
+
 
 print("-- Step 1:     Change local repository to develop branch --")
-ahead = subprocess.check_output(["git","checkout","develop"]).decode("utf-8")
+ahead = checkout_branch("develop")
 print(ahead + "\n\n")
 
 print("-- Step 2:     Show Status of develop branch --")
-notCommited = subprocess.check_output(["git","status","-s"]).decode("utf-8")
-print(notCommited + "\n")
-if notCommited is not '' :
-    eingabe = input("Should the changes be commited to local repository? [Y/N]: ")
-    if eingabe is "Y" :
-        message = input("Please enter commit message: ")
-        subprocess.call(["git","commit","-a","-m",message])
+commit_changes()
 print("\n\n")
 
 print("-- Step 3:     Pull current version of develop from GitHub --")
