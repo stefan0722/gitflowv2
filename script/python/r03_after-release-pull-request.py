a6ee9321a684d60dba897399c49332ad751b3afb
stefan0722 <stefan.henneberger@salzburgresearch.at>
2017-10-24 13:36:56 +0200

    project version

--- script/python/r03_after-release-pull-request.py
+++ script/python/r03_after-release-pull-request.py
@@ -36,6 +36,7 @@ git_flow_func.get_clean_branch_state("develop")
 print("\n-- Step 8:       Merge " + branch_name + "to develop")
 git_flow_func.merge_branch_no_ff(branch_name)
 
+print("\n-- Current Version: " + git_flow_func.get_project_version())
 print("\n-- Step 9:     Increasing version of develop to next version")
 git_flow_func.increase_branch_version(True)
 
